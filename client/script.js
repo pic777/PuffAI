a1b905db09dff8dc37f95d8166b00e56f1372f45
pic777 <picseven@gmail.com>
2023-04-13 16:05:20 +0800

    update url

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch response from the server
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://puffai.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
